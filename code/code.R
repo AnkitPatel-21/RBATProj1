63917ae3092c4d9f6b48f9999f458dbc2b0058ef
mcunningham12 <maxcunningham2001@gmail.com>
2022-01-20 20:23:56 -0600

    ankit and max 1/20 meeting work on filling in NAs

--- code/code.R
+++ code/code.R
@@ -5,3 +5,22 @@ pitches <- read_csv("pitchdata.csv")
 pitches$pa_id <- cumsum(c(0,as.numeric(diff(pitches$PAofInning))!=0))
 
 View(pitches)
+
+fill_speed <- function(pa_id){
+  p = pitches[pitches$pa_id == pa_id,]
+  if('InPlay' %in% p$PitchCall){
+    p$ExitSpeed[1:(dim(p)[1]-1)] <- p$ExitSpeed[p$PitchCall=="InPlay"]
+    p$Angle[1:(dim(p)[1]-1)] <- p$Angle[p$PitchCall=="InPlay"]
+  } else{
+    p$ExitSpeed[1:(dim(p)[1])] <- NA
+    p$Angle[1:(dim(p)[1])] <- NA
+  }
+  p
+}
+
+pas = 1:50681
+
+for(i in pas){
+  p = fill_speed(i)
+  updated = rbind(p, fill_speed(i))
+}
