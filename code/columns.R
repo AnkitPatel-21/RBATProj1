579db9747ac73baa99e170c43c4d185f2165fa3a
AnkitPatel-21 <72523102+AnkitPatel-21@users.noreply.github.com>
2022-02-18 13:19:16 -0600

    Delete code directory

--- code/columns.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-pbp <- read_csv("~/Downloads/trackman.csv")
-
-pbp1 <- select(pbp, PAofInning, PitchofPA, PitcherThrows, BatterSide, 
-               Balls, Strikes, TaggedPitchType, AutoPitchType, PitchZone, 
-               KorBB, ExitSpeed, Angle)
-
-write_csv(pbp1, "pitchdata.csv")
